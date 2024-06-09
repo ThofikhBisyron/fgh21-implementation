8a37104907cdccbb3a4d88eda03133f544db98a3
Thofikh Bisyron <162830476+ThofikhBisyron@users.noreply.github.com>
2024-06-10 00:27:45 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,13 +1,28 @@
-function cekPalindrom(kata) {
-    let split = kata.split('')
-    let reverse = split.reverse('')
-    let join = reverse.join("")
+// function cekPalindrom(kata) {
+//     let split = kata.split('')
+//     let reverse = split.reverse('')
+//     let join = reverse.join("")
 
-    if (join === kata){
-        return ('palindrom')
-    }
-    if(join != kata)
-        return ('bukan palindrom')
+//     if (join === kata){
+//         return ('palindrom')
+//     }
+//     if(join != kata)
+//         return ('bukan palindrom')
     
+// }
+// console.log(cekPalindrom('kodok'))
+
+function cekPalindrom(kata){
+    let panjangkata = kata.length
+    let hasil = ''
+        for ( let i = panjangkata - 1; i >= 0; i-- ){
+            hasil = hasil + kata[i]
+        }
+        if (kata === hasil){
+            return('Palindrom')
+        }else{
+            return('Bukan Palindrom')
+        }1
+
 }
 console.log(cekPalindrom('kodok'))
--- task2.js
+++ task2.js
@@ -1,8 +1,47 @@
-function reverseWords(kata) {
-    let split = kata.split(' ')
-    let reverse = split.reverse('')
-    let join = reverse.join(" ")
-    return join
+// function reverseWords(kata) {
+//     let split = kata.split(' ')
+//     let reverse = split.reverse('')
+//     let join = reverse.join(" ")
+//     return join
    
-}
-console.log(reverseWords('apakah sudah malam'))
+// }
+// console.log(reverseWords('apakah sudah malam'))
+
+function reverseWords(kata){
+    let panjangkata = kata.length
+    let hasil = ''
+    let akhirkata = panjangkata
+
+
+        for ( let i = panjangkata - 1; i >= 0; i-- ){
+            if (kata[i] === ' ' || i === 0){
+                let awalkata
+                if (i === 0){
+                     awalkata = i
+                }else{
+                    awalkata = i + 1
+                }
+                // let awalkata = if (i === 0) {
+                //     i = i + 1
+                // }  
+                // i = 0 jawaban 0
+                // i = 1 jawaban 2
+                // i = 2 jawaban 3
+            
+            for ( let j = awalkata; j < akhirkata; j++){
+                hasil = hasil + kata[j]
+            }
+            if (i > 0){
+                hasil = hasil + ' '  
+            }
+            akhirkata = i
+            }
+            
+        }
+        return hasil
+    }
+       
+
+
+console.log(reverseWords('Aku Adalah Saya'))
+
